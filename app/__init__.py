ac4b7680e1e1d11a0a814fd5138a6c0f7b6dca88
abuzarkhan <abuzxrkhan@gmail.com>
2024-05-24 01:55:45 +0500

    updated .env

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from .routes import main_bp
+from .models import db, User  # Import db from models
+from .auth import auth_bp  # Ensure this import is correct if you have an 'auth' blueprint
+import os
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    
+    db.init_app(app)
+    
+    login_manager = LoginManager(app)
+    login_manager.login_view = 'auth.login' 
+    login_manager.login_message_category = 'info'
+    
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')  # Register with a prefix if needed
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    return app
